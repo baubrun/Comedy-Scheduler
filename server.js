3f27a99b521c7ab7337426ec2ea66de77b04ccb4
baubrun <baubelf@gmail.com>
2020-06-16 17:47:38 -0400

    local v heroku

--- server.js
+++ server.js
@@ -10,13 +10,13 @@ const cors = require("cors")
  Middleware 
  ==============*/
 
-app.use(cors())
 app.use(userRouter)
 app.use(eventRouter)
 app.use(purchaseRouter)
 app.use(express.json())
 app.use("/", express.static("uploads"))
 app.use("/", express.static("build"))
+app.use(cors())
 
 
 
